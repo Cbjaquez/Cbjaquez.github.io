d3362bc00d1afc456fe6607b79ebe4641373d501
Christian Jaquez <109754003+Cbjaquez@users.noreply.github.com>
2023-03-01 14:19:19 -0700

    Add files via upload

---
+++ decodedscript.js
@@ -0,0 +1,9 @@
+var input = document.getElementById("userInput").value;
+
+    if (input === "password123") {
+        alert('Good job, the password is "password123!" here is the flag: BST{BR_U73_F0RC3}" );
+    }
+        else {
+            alert("Incorrect password.");
+
+    }
