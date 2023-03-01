0f58078d1086766119587d88935cacfbff251277
Christian Jaquez <109754003+Cbjaquez@users.noreply.github.com>
2023-03-01 14:37:58 -0700

    Delete decodedscript.js

--- decodedscript.js
+++
@@ -1,9 +0,0 @@
-var input = document.getElementById("userInput").value;
-
-    if (input === "password123") {
-        alert('Good job, the password is "password123!" here is the flag: BST{BR_U73_F0RC3}" );
-    }
-        else {
-            alert("Incorrect password.");
-
-    }
