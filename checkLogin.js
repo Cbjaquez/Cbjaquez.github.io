ecd3998ad79ba661aa56c1cd78327efce19ebb3e
Christian Jaquez <109754003+Cbjaquez@users.noreply.github.com>
2023-03-02 05:16:06 -0700

    Update checkLogin.js

--- checkLogin.js
+++ checkLogin.js
@@ -6,18 +6,13 @@ document.addEventListener("DOMContentLoaded", function() {
     var message = document.getElementById("message");
     var formData = new FormData(event.target);
 
-    var queryStr = "user=" + encodeURIComponent(username) + "&pass=" + encodeURIComponent(password);
-
     if (username === "Layne" && password === "111111") {
       var flag = "BST{BR_U73_F0RC3}";
-      message.innerHTML = "Correct username and password!! the flag is " + flag;
       queryStr += "&flag=" + encodeURIComponent(flag);
+      message.innerHTML = "Correct username and password!! the flag is " + flag;
     } else {
       message.innerHTML = "Incorrect username and/or password";
     }
-
-    setTimeout(function() {
-      window.location.search = queryStr;
-    }, 3000);
+    var queryStr = "user=" + encodeURIComponent(username) + "&pass=" + encodeURIComponent(password);
   });
 });
