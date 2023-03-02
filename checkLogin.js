3e6e8a0612ae8437e725cbbf56bee7d1269eb1b3
Christian Jaquez <109754003+Cbjaquez@users.noreply.github.com>
2023-03-02 05:51:57 -0700

    Update checkLogin.js

--- checkLogin.js
+++ checkLogin.js
@@ -6,12 +6,16 @@ document.addEventListener("DOMContentLoaded", function() {
     var message = document.getElementById("message");
     var formData = new FormData(event.target);
 
+     var queryStr = "user=" + encodeURIComponent(username) + "&pass=" + encodeURIComponent(password);
+    
     if (username === "Layne" && password === "111111") {
       // Redirect to success page and display success message
       window.location.href = "success.html?status=success";
     } else {
       // Redirect to error page and display error message
       window.location.href = "error.html?status=error";
+      window.location.href - "index.html"
+      queryStr += "&message=" + encodeURIComponent(message);
     }
   });
 });
